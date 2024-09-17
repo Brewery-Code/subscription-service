8ea8a9a6cbbc45eccc77bfbfb91efa75d3da6a44
Vitalii <x4gm9k@gmail.com>
2024-09-17 23:56:21 +0300

    Додано модальне вікно для входу в обліковий запис

--- frontend/src/components/Header/Nav/Buttons/LogIn/LogIn.jsx
+++ frontend/src/components/Header/Nav/Buttons/LogIn/LogIn.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
+import LogInModal from '../../../../Modals/LogIn/LogInModal';
+
 import styles from './LogIn.module.scss';
 
 export default function LogIn() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen((prev) => (!prev));
+  }
+
   return (
-    <button className={styles['log-in']}>Log in</button>
+    <>
+      <button className={styles['log-in']} onClick={toggleModal}>Log in</button>
+      <LogInModal isOpen={isModalOpen} toggleModal={toggleModal} />
+    </>
   )
 }
